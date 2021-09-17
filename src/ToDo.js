cae9ebcd629b4843f3fc801f4b08a0ba50de0bb4
Erol Aliyev <erol_aliyev@epam.com>
2021-09-17 11:23:14 +0200

    Create ToDo.js

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import Task from './components/Task'
+import CreateTask from './components/CreateTask'
 import './ToDo.css'
 
-const Task = (task) => {
-  return (
-    <div
-      className="task"
-      style={{ textDecoration: task.task.completed ? "line-through" : ""}}
-    >
-      {task.task.title}
-    </div>
-  )
-}
-
 const ToDo = () => {
-  const [tasks, setTasks] = useState([
-    {
-      title: "Create to do app",
-      completed: false
-    },
-    {
-      title: "Go to gym",
-      completed: true
-    }
-  ]);
+  const [tasks, setTasks] = useState(() => localStorage.getItem("tasks") === null ? [] : JSON.parse(localStorage.getItem("tasks")) );
+
+  const addTask = title => {
+    const newTasks = [...tasks, {title, completed: false}];
+    setTasks(newTasks);
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+  }
+
+  const completeTask = index => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = true;
+    setTasks(newTasks);
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+  }
+
+  const removeTask = index => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+  }
 
   return (
     <div className="todo-container">
-      <div className="header">TODO - ITEMS</div>
+      <div className="header"> To Dos... </div>
       <div className="tasks">
         {tasks.map((task, index) => (
           <Task 
             task={task}
-            key={index}
             index={index}
+            completeTask={completeTask}
+            removeTask={removeTask}
+            key={index}
           />
         ))}
+      </div>
+      <div className="create-task">
+        <CreateTask addTask={addTask} />
       </div>
     </div>
   );
